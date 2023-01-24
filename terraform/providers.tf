1bf586042a128ccd32c488693d72f6cdb58c168c
Gianluca Natali <8244206+gianlucanatali@users.noreply.github.com>
2023-01-24 21:45:49 +0100

    update providers versions

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,11 +2,11 @@ terraform {
     required_providers {
         aws = {
             source  = "hashicorp/aws"
-            version = "4.46"
+            version = "4.51.0"
         }
         confluent = {
             source = "confluentinc/confluent"
-            version = "1.23.0"
+            version = "1.25.0"
         }
     }
 }
