5f1ecde09686450604563479c8238b7c7fd346c2
Gianluca Natali <8244206+gianlucanatali@users.noreply.github.com>
2023-01-24 21:47:50 +0100

    passing confluent provider api keys via tfvars

--- terraform/confluent.tf
+++ terraform/confluent.tf
@@ -1,3 +1,8 @@
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key    # optionally use CONFLUENT_CLOUD_API_KEY env var
+  cloud_api_secret = var.confluent_cloud_api_secret # optionally use CONFLUENT_CLOUD_API_SECRET env var
+}
+
 resource "random_id" "env_display_id" {
     byte_length = 4
 }
